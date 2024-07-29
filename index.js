7a88ad0a9be438dec61525e4664b4d55844ca2a7
tqkdev <khaitran22102017@gmail.com>
2024-07-29 17:43:26 +0700

    .

--- index.js
+++ index.js
@@ -127,6 +127,10 @@ app.use('/api/order', orderRoute);
 
 const port = process.env.PORT || 3001;
 
+app.get('/', (req, res) => {
+    res.send('Server is running....');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
